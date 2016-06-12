880657b8e1d4b548ac2c9946dfcf170e47bafeb3
Shemar Lindie <shemarlindie@gmail.com>
2016-06-12 10:32:13 -0400

    Add syntax highlighting to markdown code snippets.

--- src/modules/app.issue/issue.module.js
+++ src/modules/app.issue/issue.module.js
@@ -3,6 +3,19 @@
   'use strict';
   
   // MODULE DEFINITION
-  angular.module('app.issue', ['hc.marked']);
+  angular.module('app.issue', ['hc.marked'])
+    .config(['markedProvider', function (markedProvider) {
+      markedProvider.setOptions({
+        gfm: true,
+        breaks: true,
+        highlight: function (code, lang) {
+          if (lang) {
+            return hljs.highlight(lang, code, true).value;
+          } else {
+            return hljs.highlightAuto(code).value;
+          }
+        }
+      });
+    }]);
 
 })(angular);
