824a26a18adb6c5b9ab8db16fc9610404d725eb0
Shemar Lindie <shemarlindie@gmail.com>
2021-09-10 16:21:04 -0400

    build

--- dist/modules/app/tag.service.js
+++ dist/modules/app/tag.service.js
@@ -1,2 +1,2 @@
-!function(t){"use strict";t.module("app").factory("TagService",["AppConfig","$http",function(t,n){var e=t.API_URI,r={all:function(t){return n.get(e+"/tags/?"+$.param(t))},search:function(t){return n.get(e+"/tags/?search="+encodeURIComponent(t))}};return r}])}(angular);
+!function(t){"use strict";t.module("app").factory("TagService",["AppConfig","$http",function(t,n){var e=t.API_URI+"/issue",r={all:function(t){return n.get(e+"/tags/?"+$.param(t))},search:function(t){return n.get(e+"/tags/?search="+encodeURIComponent(t))}};return r}])}(angular);
 //# sourceMappingURL=../sourcemaps/app/tag.service.js.map
